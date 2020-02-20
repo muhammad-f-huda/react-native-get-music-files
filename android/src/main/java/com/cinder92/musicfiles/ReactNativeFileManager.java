1dade25757c7bccf71023a99898ab6a0eb4e55fd
muhammad f huda <52238215+muhammad-f-huda@users.noreply.github.com>
2020-02-20 14:21:29 +0700

    rewrite

--- android/src/main/java/com/cinder92/musicfiles/ReactNativeFileManager.java
+++ android/src/main/java/com/cinder92/musicfiles/ReactNativeFileManager.java
@@ -1,66 +1,54 @@
 package com.cinder92.musicfiles;
 
+import android.app.Application;
 import android.graphics.Bitmap;
-import android.util.Base64;
 import android.util.Log;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
-import android.os.Environment;
+import java.io.File;
 
 /**
  * Created by dantecervantes on 28/11/17.
  */
 
-public class ReactNativeFileManager extends ReactNativeBlurImage{
-    public String saveImageToStorageAndGetPath(long songId, Bitmap songImage) throws IOException {
-        try{
+public class ReactNativeFileManager extends Application {
 
-            if (songImage != null) {
-                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                songImage.compress(Bitmap.CompressFormat.JPEG, 80, byteArrayOutputStream);
-                byte[] byteArray = byteArrayOutputStream.toByteArray();
-                // String encodedImage = Base64.encodeToString(byteArray, Base64.DEFAULT);
-                // byte[] imageByte = Base64.decode(encodedImage, Base64.DEFAULT);
+    public void saveImageToStorage(String path, Bitmap songImage) throws IOException {
+        try {
 
-                if(byteArray != null) {
-                    return saveToStorage(songId, byteArray);
-                }
+            if(songImage!=null) {
+                ByteArrayOutputStream byteArrayOutputStream= new ByteArrayOutputStream();
+                songImage.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+                byte[] byteArray= byteArrayOutputStream.toByteArray();
+                if(byteArray!=null) saveToStorage(path, byteArray);
             }
 
-        } catch (IOException e){
-            Log.e("Error savingImageAfter",e.getMessage());
+        } catch(IOException e){
+            Log.e("Error savingImageAfter", e.getMessage());
         }
-
-        return("");
-
     }
 
 
-    public String saveToStorage(long songId, byte[] imageBytes) throws IOException {
-        FileOutputStream fos = null;
+    public void saveToStorage(String path, byte[] imageBytes) throws IOException {
+        FileOutputStream fileOutputStream= null;
         try {
-            String pathToImg = Environment.getExternalStorageDirectory() + "/MusicApp/cover/";
-            File folderPath = new File(pathToImg);
-            folderPath.mkdirs();
-            pathToImg+= songId;
-            File filePath = new File(pathToImg);
-            fos = new FileOutputStream(filePath, true);
-            fos.write(imageBytes);
-            return("file://" + pathToImg);
-        } catch (IOException e){
+
+            File filePath= new File(path);
+            File basepath= filePath.getParentFile();
+            basepath.mkdirs();
+            fileOutputStream= new FileOutputStream(filePath, true);
+            fileOutputStream.write(imageBytes);
+
+        } catch(IOException e) {
             Log.e("Error saving image => ", e.getMessage());
         } finally {
-            if (fos != null) {
-                fos.flush();
-                fos.close();
+            if(fileOutputStream!=null) {
+                fileOutputStream.flush();
+                fileOutputStream.close();
             }
         }
-
-        return("");
-
     }
+
 }
